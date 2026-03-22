7e1f4979e810514a201fb272a736d023003dc5e5
kirill_kamyshanov <nestor10000@mail.ru>
2026-03-22 14:03:24 +0300

    added parametrize for POST and PUT endpoints

--- tests/test_API_memes.py
+++ tests/test_API_memes.py
@@ -3,13 +3,46 @@
 # 3 GET /meme получение списка всех мемов              ПОЗ + НЕГ -
 # 4 GET /meme/<id> Получение одного мема по id         ПОЗ + НЕГ -
 # 5 POST /meme Добавление нового мема                  ПОЗ + НЕГ -
-# 6 PUT /meme/<id> Изменение существующего мема        ПОЗ - НЕГ -
+# 6 PUT /meme/<id> Изменение существующего мема        ПОЗ + НЕГ -
 # 7 DELETE /meme/<id> Удаление мема                    ПОЗ - НЕГ -
 
 # для позитивов добавить параметризацию
 import pytest
 user = {"name": "Wowa"}
 name = user['name']
+
+
+# тестовые данные для обновления мемов
+create_bodies_positive = [
+    {"text": "1 description","url": "https://memes/one","tags": ["first","second"],
+     "info": {"additional info": "training mem"}},
+
+    {"text": "2 description", "url": "https://memes/two", "tags": ["second"],
+     "info": {"additional info": "training mem"}},
+
+    {"text": "3 description", "url": "https://memes/three", "tags": ["third"],
+     "info": {"additional info": "training mem"}},
+
+    {"text": "4 description", "url": "https://memes/four", "tags": ["forth"],
+     "info": {"additional info": "training mem"}}
+]
+put_bodies_positive = [
+    {"text": "2_description","url": "https://memes/new/put","tags": ["make a change","put"],
+     "info": {"additional info": "training 2 put"}},
+
+    {"text": "3_description","url": "https://memes/new/put","tags": ["make a change","put"],
+     "info": {"additional info": "training 3 put"}},
+
+    {"text": "4_description", "url": "https://memes/new/put", "tags": ["make a change", "put"],
+     "info": {"additional info": "training 4 put"}},
+
+    {"text": "5_description", "url": "https://memes/new/put", "tags": ["make a change", "put"],
+     "info": {"additional info": "training 5 put"}},
+]
+
+
+
+
 
 # чистовик ПОЗ
 # def test_post_authorize(first_test_token, post_authorize):
@@ -43,20 +76,13 @@ name = user['name']
 #     get_meme_id.check_meme_id()
 
 
+
+
+
 # чистовик ПОЗ
-# def test_post_meme(post_meme, check_token):
-#     body = {
-#         "text": "description",
-#         "url": "https://memes",
-#         "tags": [
-#             "first",
-#             "second"
-#         ],
-#         "info": {
-#             "additional info": "training mem"
-#         }
-#     }
-#     post_meme.create_new_meme(check_token, body)
+# @pytest.mark.parametrize("body", create_bodies_positive)
+# def test_post_meme(post_meme, check_token, body):
+#     post_meme.create_new_meme(token=check_token, body=body)
 #     post_meme.check_that_status_code_is_200()
 #     post_meme.check_response_body_presence()
 #     post_meme.check_response_structure()
@@ -64,22 +90,13 @@ name = user['name']
 #     post_meme.delete_test_meme()
 
 
-@pytest.mark.parametrize
-def test_put_meme_id(put_meme_id, check_token, create_test_meme_then_delete):
-    new_body = {
-        "text": "new_description",
-        "url": "https://memes/new/put",
-        "tags": [
-            "make a change",
-            "put"
-        ],
-        "info": {
-            "additional info": "training put"
-        }
-    }
-    put_meme_id.put_meme_by_id(meme_id=create_test_meme_then_delete, token=check_token, body=new_body)
-    put_meme_id.check_that_status_code_is_200()
-    put_meme_id.check_response_body_presence()
-    put_meme_id.check_response_structure()
-    put_meme_id.check_meme_id()
-    put_meme_id.check_updating_meme()
+
+# чистовик ПОЗ
+# @pytest.mark.parametrize("new_body", put_bodies_positive)
+# def test_put_meme_id(put_meme_id, check_token, create_test_meme_then_delete, new_body):
+#     put_meme_id.put_meme_by_id(meme_id=create_test_meme_then_delete, token=check_token, body=new_body)
+#     put_meme_id.check_that_status_code_is_200()
+#     put_meme_id.check_response_body_presence()
+#     put_meme_id.check_response_structure()
+#     put_meme_id.check_meme_id()
+#     put_meme_id.check_updating_meme()
